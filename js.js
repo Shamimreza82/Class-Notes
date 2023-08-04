4b0ad28fc8c888ac243a738be990902a2ea879df
Shamimreza82 <shamimrezaone@gmail.com>
2023-08-04 18:50:31 +0600

    update codes

--- js.js
+++ js.js
@@ -15,23 +15,70 @@
 
 
 
-function bigNumberFind (numbers) {
-    let largest = numbers[0]; 
-    for ( let i = 1; i < numbers.length; i ++){
-        const currentItems = numbers[i]; 
-        if (currentItems > largest){
-            largest = currentItems; 
-        }
-    }
-    return largest;      
+// function bigNumberFind (numbers) {
+//     let largest = numbers[0]; 
+//     for ( let i = 1; i < numbers.length; i ++){
+//         const currentItems = numbers[i]; 
+//         if (currentItems > largest){
+//             largest = currentItems; 
+//         }
+//     }
+//     return largest;      
+// }
+
+
+
+// const myNumber = [2,6,9,4,9,1,9,13,67]
+// console.log(bigNumberFind(myNumber))
+
+
+
+
+
+// function bigFriend (name) {
+//     let largestFriend = name[0]; 
+//     for ( let i = 1; i < name.length; i++) {
+//         const friends = name[i]; 
+//         if ( largestFriend > friends) {
+            
+//         }
+//     }
+// }
+
+
+
+// const myFriend = ["arif", "rubel", "mamun", "nahamul", "shakil"]
+
+
+
+// function myFriend () 
+
+
+
+let myFriend = {
+    name: "arif", 
+    age: 34, 
+    phone: "4040404040", 
+    address: "jhenaidah"
 }
 
 
-
-const myNumber = [2,6,9,4,9,1,9,13,67]
-console.log(bigNumberFind(myNumber))
-
-
-
+// console.log(myFriend.age); 
+// console.log(myFriend["phone"]); 
+// const x = "address"; 
+// console.log(myFriend[x]); 
 
 
+const keys = Object.keys(myFriend); 
+const valu = Object.values(myFriend)
+
+
+// for(let key of keys) {
+//     console.log(myFriend[key])
+
+// }
+
+for ( let i = 1; i < keys.length; i++){
+    const key = keys[i];
+    console.log (myFriend[key])
+} 
